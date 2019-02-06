0de8e8828163ad2029a74f256f4293716ab24963
Iacob Catalin <cataliniulian_iacob@yahoo.com>
2019-02-06 11:13:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,17 @@ var menuItem;
 var difX;
 var difY;
 var body;
+var timeout;
+var requestAnimationFrame = window.requestAnimationFrame;
+var mouseX; 
+var mouseY; 
+var animationFrame;
 
 function start() {
     menuItem = document.getElementsByClassName("menuItem")[0];
     
     body = document.body;
-    body.addEventListener("mouseout", pageLeave);
+    body.addEventListener("mouseleave", pageLeave);
     addEventListeners();
 }
 
@@ -17,8 +22,12 @@ function addEventListeners() {
     menuItem.addEventListener("mousedown", menuItemMouseDown);
 
 }
-function menuItemMouseDown(e) {
-    body.addEventListener("mousemove", menuItemMouseMove);
+function menuItemMouseDown(e) {     
+    timeout = setTimeout(()=> {
+        body.addEventListener("mousemove", menuItemMouseMove);
+        animationFrame = requestAnimationFrame(update);
+    },10);
+
     body.addEventListener("mouseup", menuItemUpMove);
     difX = e.clientX - menuItem.offsetLeft;
     difY = e.clientY - menuItem.offsetTop;
@@ -26,11 +35,17 @@ function menuItemMouseDown(e) {
 
 function menuItemMouseMove(e) {
     console.log(e.clientX, e.clientY);
-    menuItem.style.top = (e.clientY - difY) + "px";
-    menuItem.style.left = (e.clientX - difX) + "px";
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
 
+function update(e){
+    menuItem.style.top = (mouseY - difY) + "px";
+    menuItem.style.left = (mouseX - difX) + "px";
+    animationFrame = requestAnimationFrame(update);
 }
 function menuItemUpMove(e) {
+    clearTimeout(timeout);
     menuItem.style.top = (e.clientY - difY) + "px";
     menuItem.style.left = (e.clientX - difX) + "px";
     body.removeEventListener("mousemove", menuItemMouseMove);
@@ -41,3 +56,7 @@ function pageLeave(){
     body.removeEventListener("mousemove", menuItemMouseMove);
     body.removeEventListener("mouseup", menuItemUpMove);
 }
+// e.target pentru 
+// lista de patrate - x la numar
+// drag and drop la ele
+// hit test -  cand doua patrate se ating se fac galbene;
